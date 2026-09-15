3fb7949523ba4e5ec2417c2c16204438e0323c0c
Samuel Asongo <samuelasongo8@gmail.com>
2026-09-15 13:15:37 +0300

    Implement Exercise 10 platform-specific font

--- src/theme.js
+++ src/theme.js
@@ -1,7 +1,18 @@
+import { Platform } from 'react-native';
+
 const colors = {
   primary: '#0366d6',
 };
 
 export default {
   colors,
+  fontSizes: {},
+  fonts: {
+    main: Platform.select({
+      android: 'Roboto',
+      ios: 'Arial',
+      default: 'System',
+    }),
+  },
+  fontWeights: {},
 };
